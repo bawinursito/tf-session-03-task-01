def65c870eac214a98b69d3c1bb8d38bb9c22433
Bayu Winursito <bayu.winursito@yitmh.com>
2024-01-11 11:58:12 +0800

    first commit

---
+++ tf-code-aws/prod/global/dns/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+###### you need to get VPC ID data from VPC workspace and use it to create DNS zones ####
+
+// use terraform_remote_state to get data from VPC workspace
+
+// use locals{} to transform the data and insert it to dns_entry variable
+
+// use dns module and pass the modifed dns_entry variable to provision DNS resources
+
+
+
+
